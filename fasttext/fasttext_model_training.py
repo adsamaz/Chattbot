4f5b39ec3f7cf08e0553a9767106a8994f7a34b9
adsamaz <33390767+adsamaz@users.noreply.github.com>
2020-01-13 11:42:44 +0100

    More training data

--- fasttext/fasttext_model_training.py
+++ fasttext/fasttext_model_training.py
@@ -1,22 +1,18 @@
 import fasttext
 # Skipgram model :¨
 import os
+model = None
 
-model = fasttext.train_supervised(input="fasttext/data.train", epoch=40, lr=1, wordNgrams=3)
+
 def predict(text):
     a = model.predict(text)
     label, confidence = ("nonsense", a[1][0]) if a[1][0] < 0.6 else (a[0][0], a[1][0])
     return (label, confidence)
-    
-print(predict("I want to leave a review"))
 
-print(predict("hello!"))
-
-print(predict("Im I a robot?"))
-
-print(model.predict("Give me some information about a game"))
+model = fasttext.train_supervised(input="fasttext/data.train", epoch=28, lr=1, wordNgrams=4)
 
 test = model.test("fasttext/data.valid")
 print("Precision: " + str(test[1]*100) + "%")
+
 
 model.save_model("fasttext/trained_model.bin")
